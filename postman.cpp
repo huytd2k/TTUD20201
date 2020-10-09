507da3f39ed7f4822e52c859582dcb712c923b95
Huy Tran.D <huy.td183768@sis.hust.edu.vn>
2020-10-09 19:27:45 +0700

    add locate

--- postman.cpp
+++ postman.cpp
@@ -1,1 +1,21 @@
+
 #include <bits/stdc++.h>
+#define MP make_pair
+#define F first
+#define S second
+#define MAX 1000
+using namespace std;
+typedef pair< int, int > p;
+typedef long long ll;
+
+int main() {
+    int n, k;
+    vector< p > v;
+    cin >> n >> k;
+    for (int i=0; i < n; ++i) {
+        int x, m;
+        cin >> x >> m;
+        v.push_back( MP(x,m) );
+    }
+    sort(v.begin(), v.end());   
+}
